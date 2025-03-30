43397db05e0740fa80b617a3694862145965d8bf
abbtr <amir.baibatyrov.work@gmail.com>
2025-03-30 22:02:07 +0500

    Lets go, so far

--- src/telegram/tg.py
+++ src/telegram/tg.py
@@ -1,7 +1,7 @@
 from telegram.config import app_config
 from modules.common import start
 from telegram.ext import ApplicationBuilder, CommandHandler, CallbackQueryHandler, MessageHandler, filters
-
+from modules.message_handler import text_message_handler
 def create_bot_application():
     """Sets up the bot with all the necessary handlers."""
     app_config.configure_logging()
@@ -11,7 +11,7 @@ def create_bot_application():
     # Register common handlers
     application.add_handler(CommandHandler('start', start))
 
-    application.add_handler(MessageHandler(filters.TEXT, start_game_custom))
+    application.add_handler(MessageHandler(filters.TEXT, m))
 
     application.add_handler(CallbackQueryHandler(start_game_word_day, pattern='^gamemode->word_day$'))
     application.add_handler(CallbackQueryHandler(choice_custom, pattern='^gamemode->custom$'))
